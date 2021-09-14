4444efa6728991c024223378ef865d9b57d24e1b
Achintya <achintyatiwari27>
2021-09-15 01:08:26 +0530

    [Refactored]:Solved using switch statement

--- UC4_Switch_statement.java
+++ UC4_Switch_statement.java
@@ -21,18 +21,25 @@ public class EmpWage {
     }
 
     public static void dailyEmployeeWage() {
+        int empHrs;
         int WAGE_PER_HOUR = 20;
         int FULL_DAY_HOUR = 8;
         int PART_TIME_HOUR = 4;
         System.out.println("\nCheck the employee daily wage:");
         int fullpresent = WAGE_PER_HOUR * FULL_DAY_HOUR;
         int partTime = WAGE_PER_HOUR * PART_TIME_HOUR;
-        if (check == 1) {
-            System.out.println("\n Employee is present for full day, the salary is " + fullpresent);
-        } else if (check == 2) {
-            System.out.println("\n  Employee is present part time, the salary is " + partTime);
-        } else {
-            System.out.println("Employee is absent so there wil be no generation of salary");
+        switch (check) {
+            case 1 :
+                System.out.println("\n Employee is present for full day, the salary is " + fullpresent);
+                check = 1;
+                break;
+            case 2 :
+                System.out.println("\n  Employee is present part time, the salary is " + partTime);
+                check = 2;
+                break;
+            default:
+                System.out.println("Employee is absent so there wil be no generation of salary");
+                empHrs = 0;
         }
     }
 }
